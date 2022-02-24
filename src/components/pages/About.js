52d055c7e0211dd72aa42d4dfb6658f7c86f7c08
eric-muhire <e.muhire24@gmail.com>
2022-02-24 06:12:28 +0100

    add about, contact, home and product pages

---
+++ src/components/pages/About.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function About() {
+    return(
+        <div className="container">
+            <div class="card mt-4">
+                 <div class="card-body">
+                     <h2>Om mig</h2>
+
+
+                     </div>
+                 </div>
+            </div>
+    );
+
+}
+export default About;
---
+++ src/components/pages/Products.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function Products() {
+    return(
+        <div className="container">
+            <div class="card mt-4">
+                 <div class="card-body">
+                     <h2>Produkter</h2>
+
+
+                     </div>
+                 </div>
+            </div>
+    );
+
+}
+export default Products;
