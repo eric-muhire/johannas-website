3d46512259726b8e16cc3a976be7ac63639a1e5d
eric-muhire <e.muhire24@gmail.com>
2022-03-08 09:12:58 +0100

    fix section on for product
    add:secure form on the contact page so that user has 20 characters for name

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -22,7 +22,8 @@ function About() {
                      <div className="container">
                          <h5 className="main-heading">Varför Aloe Vera</h5>
                          <div className="underline"></div>
-                         <p>dfsmlsdkfskldflskdsd</p>
+                         <p>simply dummy text of the printing and typesetting industry.'\n' Lorem Ipsum has been the industry's standard dummy text ever 
+                             since the 1500s, when an unknown printer took a galley of type</p>
                      </div>
                  </section>
                  <VisionMission/>
